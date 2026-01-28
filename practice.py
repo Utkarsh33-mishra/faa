7836be51d73e1b7d59135ad1c6b7fe9ab779f3b1
Utkarsh Mishra <um098178@gmail.com>
2026-01-28 16:08:25 +0530

    add python practice files

---
+++ practice.py
@@ -0,0 +1,19 @@
+# character_name ="Ram"
+# character_age = 20
+# print(f"he is good {character_name}\tAnd his age is {character_age}")
+# k = "yu"
+# print(k.upper() + "good buy")
+# print(k.isupper())
+# print(len(k))
+# print(k[1])
+# print(k.index("y"))
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+# character_name ="ton"
+# character_age = 15.67
+# print("There once was a man named" + character_name +",")
+# print("He is" + str(character_age )+"," )
+k = "ram"
+print(k.index("m"))
