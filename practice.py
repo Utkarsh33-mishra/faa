f3a916da212ab9498d3779d650911d72b539145d
Utkarsh Mishra <um098178@gmail.com>
2026-01-29 05:54:48 +0530

    Initial commit :added calculator and practice file

--- practice.py
+++ practice.py
@@ -15,5 +15,17 @@
 # character_age = 15.67
 # print("There once was a man named" + character_name +",")
 # print("He is" + str(character_age )+"," )
-k = "ram"
-print(k.index("m"))
+# k = "ram"
+# print(k.replace("ram","shyam"))
+#  play with number
+# my_num = -5
+# print(abs(my_num))
+# print(10 %3)
+# print(pow(5,6))
+# print(min(6,7))
+# print(round(6.7))
+# from math import *
+# print(sqrt(36))
+# name = input("Enter your name: ")
+# age = input("Enter your age")
+# print("Hello" +name+ "! You are" + age)
