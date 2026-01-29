c8348ce3bfa8f2856a355c3275069bd53c923681
Utkarsh Mishra <um098178@gmail.com>
2026-01-29 18:35:32 +0530

    k

--- practice.py
+++ practice.py
@@ -29,3 +29,28 @@
 # name = input("Enter your name: ")
 # age = input("Enter your age")
 # print("Hello" +name+ "! You are" + age)
+# print("Mad libs Game" + str(1)+"'")
+# print()
+# color = input("Enter a color :")
+# plural_noun = input("Enter a plural Noun :")
+# celebrity = input("Enter a celebrity :")
+
+# print("Roses are" + color)
+# print(plural_noun + "are blue")
+# print("I love" + celebrity)
+# print()
+# print("Mad libs Game" + str(2)+"'")
+# risk = input("Enter a risk :")
+# Game= input("Enter a Game :")
+# opportunity = input("Enter a opportunity :")
+
+# print("lifes are made with " + risk)
+# print("utkarsh is going to make"+ opportunity)
+# print("Joy it make it happen" +Game)
+
+# print(list)
+
+# friends =["Kevin","Karen","Jim"]
+# friends[1]="Mike"
+# print(friends[1:2])
+
